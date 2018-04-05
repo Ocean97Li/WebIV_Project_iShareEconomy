d5dd70dbed39eb7b50470a2317bd2dfccf457095
HU LI <hu.li.y1090@student.hogent.be>
2018-04-05 18:27:59 +0200

    update google maps: custom style
    custom style toegevoegd aan agm component

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
+import { LatLngBounds, AgmMap } from '@agm/core';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,109 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My first AGM project';
+  title = 'iShareEconomy';
   lat = 51.053645;
   lng = 3.721506;
   zoom = 18;
+  streetViewControl = false;
+  zoomControl = false;
+  styles = [
+    {
+      'featureType': 'poi.business',
+      'stylers': [
+        {
+          'visibility': 'off'
+        }
+      ]
+    },
+    {
+      'featureType': 'poi.medical',
+      'stylers': [
+        {
+          'visibility': 'off'
+        }
+      ]
+    },
+    {
+      'featureType': 'poi.park',
+      'elementType': 'geometry.fill',
+      'stylers': [
+        {
+          'color': '#a1e09a'
+        }
+      ]
+    },
+    {
+      'featureType': 'road.arterial',
+      'elementType': 'labels.icon',
+      'stylers': [
+        {
+          'visibility': 'off'
+        }
+      ]
+    },
+    {
+      'featureType': 'road.highway',
+      'elementType': 'labels.icon',
+      'stylers': [
+        {
+          'visibility': 'off'
+        }
+      ]
+    },
+    {
+      'featureType': 'transit.station.bus',
+      'stylers': [
+        {
+          'visibility': 'off'
+        }
+      ]
+    },
+    {
+      'featureType': 'transit.station.bus',
+      'elementType': 'labels',
+      'stylers': [
+        {
+          'visibility': 'off'
+        }
+      ]
+    },
+    {
+      'featureType': 'transit.station.bus',
+      'elementType': 'labels.text',
+      'stylers': [
+        {
+          'visibility': 'off'
+        }
+      ]
+    },
+    {
+      'featureType': 'transit.station.rail',
+      'elementType': 'labels.text',
+      'stylers': [
+        {
+          'visibility': 'off'
+        }
+      ]
+    },
+    {
+      'featureType': 'water',
+      'elementType': 'geometry.fill',
+      'stylers': [
+        {
+          'color': '#638b9d'
+        }
+      ]
+    },
+    {
+      'featureType': 'water',
+      'elementType': 'labels.text',
+      'stylers': [
+        {
+          'color': '#dfeff6'
+        }
+      ]
+    }
+  ];
+
 }
